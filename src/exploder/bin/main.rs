ffecc03f63e765b9226a5b555e2c14d973f2aeed
James Smith <james@foxglove.dev>
2025-04-15 08:10:08 +1000

    simplify the exploder

--- src/exploder/bin/main.rs
+++ src/exploder/bin/main.rs
@@ -2,10 +2,10 @@ use std::{collections::BTreeMap, fs::File, io::Write};
 
 const BOOK: &str = include_str!("iliad.txt");
 const JAN_01_2000_MS: u64 = 946645200000;
-const REPEAT_N: u64 = 100;
+const REPEAT_N: u64 = 200;
 
 fn write_line(file: &mut File, timestamp: u64, line: &str) {
-    writeln!(file, "{timestamp} | {line}").expect("failed write");
+    writeln!(file, "{timestamp}|{line}").expect("failed write");
 }
 
 fn main() {
@@ -17,25 +17,22 @@ fn main() {
 
     for n in 0..REPEAT_N {
         if n % 10 == 0 {
-            println!("writing out the Iliad for the {n}th time...");
+            println!("Writing out the Iliad for the {n}th time...");
         }
         for line in BOOK.lines() {
-            let Some(first_character) = line.chars().next() else {
-                continue;
-            };
-            let lowercase = first_character.to_ascii_lowercase();
-            if !lowercase.is_alphabetic() {
-                let file = other_file.get_or_insert_with(|| {
+            let first_letter = line.chars().next().filter(|c| c.is_alphabetic());
+            let file = match first_letter {
+                None => other_file.get_or_insert_with(|| {
                     File::create("data/other.txt").expect("failed to create other.txt")
-                });
-                write_line(file, cur_timestamp, line);
-                continue;
-            }
-            let file = outputs.entry(lowercase).or_insert_with(|| {
-                File::create(format!("data/{lowercase}.txt")).expect("failed to create letter file")
-            });
+                }),
+                Some(letter) => outputs.entry(letter).or_insert_with(|| {
+                    File::create(format!("data/{letter}.txt"))
+                        .expect("failed to create letter file")
+                }),
+            };
             write_line(file, cur_timestamp, line);
             cur_timestamp += rand::random_range(1u64..1000u64);
         }
     }
+    println!("Wrote out the Iliad {REPEAT_N} times.")
 }
