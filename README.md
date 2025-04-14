a0f5244a31b71a7295695ccad6d7fa8415ec8551
James Smith <james@foxglove.dev>
2025-04-14 16:08:39 +1000

    repeat many times

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-Iliad plaintext copied from [here](https://www.gutenberg.org/files/6150/6150-h/6150-h.htm).
+## Text Merge Challenge
+
+Cointains harness code and boilerplate for a timestamp-merge coding challenge.
+
+## Usage
+
+To populate the input data files, run:
+
+```
+cargo run --bin exploder
+```
+
+To run the merging service, run:
+
+```
+cargo run --bin service
+```
+
+# Credits
+
+Iliad plaintext sourced from [Project Gutenberg](https://www.gutenberg.org/files/6150/6150-h/6150-h.htm).
--- src/exploder/bin/main.rs
+++ src/exploder/bin/main.rs
@@ -2,6 +2,7 @@ use std::{collections::BTreeMap, fs::File, io::Write};
 
 const BOOK: &str = include_str!("iliad.txt");
 const JAN_01_2000_MS: u64 = 946645200000;
+const REPEAT_N: u64 = 100;
 
 fn write_line(file: &mut File, timestamp: u64, line: &str) {
     writeln!(file, "{timestamp} | {line}").expect("failed write");
@@ -14,22 +15,27 @@ fn main() {
 
     std::fs::create_dir_all("data").expect("failed to create data dir");
 
-    for line in BOOK.lines() {
-        let Some(first_character) = line.chars().next() else {
-            continue;
-        };
-        let lowercase = first_character.to_ascii_lowercase();
-        if !lowercase.is_alphabetic() {
-            let file = other_file.get_or_insert_with(|| {
-                File::create("data/other.txt").expect("failed to create other.txt")
+    for n in 0..REPEAT_N {
+        if n % 10 == 0 {
+            println!("writing out the Iliad for the {n}th time...");
+        }
+        for line in BOOK.lines() {
+            let Some(first_character) = line.chars().next() else {
+                continue;
+            };
+            let lowercase = first_character.to_ascii_lowercase();
+            if !lowercase.is_alphabetic() {
+                let file = other_file.get_or_insert_with(|| {
+                    File::create("data/other.txt").expect("failed to create other.txt")
+                });
+                write_line(file, cur_timestamp, line);
+                continue;
+            }
+            let file = outputs.entry(lowercase).or_insert_with(|| {
+                File::create(format!("data/{lowercase}.txt")).expect("failed to create letter file")
             });
             write_line(file, cur_timestamp, line);
-            continue;
+            cur_timestamp += rand::random_range(1u64..1000u64);
         }
-        let file = outputs.entry(lowercase).or_insert_with(|| {
-            File::create(format!("data/{lowercase}.txt")).expect("failed to create letter file")
-        });
-        write_line(file, cur_timestamp, line);
-        cur_timestamp += rand::random_range(1u64..1000u64);
     }
 }
